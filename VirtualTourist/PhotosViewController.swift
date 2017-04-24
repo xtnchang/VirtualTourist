7a544e49ca48aaf56c804b30821eafcc6bb4d417
Christine Chang <xtnchang@gmail.com>
2017-04-24 12:58:25 -0700

    Add mapView and collectionView
    Added to PhotosViewController
    -Update storyboard
    -Connect IBOutlets

--- VirtualTourist/PhotosViewController.swift
+++ VirtualTourist/PhotosViewController.swift
@@ -8,29 +8,18 @@
 
 import Foundation
 import UIKit
+import MapKit
 
 class PhotosViewController: UIViewController {
 
+    @IBOutlet weak var mapView: MKMapView!
+    
+    @IBOutlet weak var collectionView: UICollectionView!
+    
+    
     override func viewDidLoad() {
-        super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
