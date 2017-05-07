fbe8354bbb46eaa0c632d3ac03fc8b23f40f0d99
Christine Chang <xtnchang@gmail.com>
2017-05-06 17:44:45 -0700

    Add Core Data entity initializers

--- VirtualTourist/Photo+CoreDataClass.swift
+++ VirtualTourist/Photo+CoreDataClass.swift
@@ -12,4 +12,15 @@ import CoreData
 @objc(Photo)
 public class Photo: NSManagedObject {
 
+    convenience init(imageData: NSData, context: NSManagedObjectContext) {
+        
+        // An EntityDescription is an object that has access to all the information you provided in the Entity part of the model.
+        // You need it to create an instance of this class.
+        if let ent = NSEntityDescription.entity(forEntityName: "Photo", in: context) {
+            self.init(entity: ent, insertInto: context)
+            self.imageData = imageData
+        } else {
+            fatalError("Unable to find Entity name!")
+        }
+    }
 }
--- VirtualTourist/Pin+CoreDataClass.swift
+++ VirtualTourist/Pin+CoreDataClass.swift
@@ -12,4 +12,16 @@ import CoreData
 @objc(Pin)
 public class Pin: NSManagedObject {
 
+    convenience init(latitude: Double, longitude: Double, context: NSManagedObjectContext) {
+        
+        // An EntityDescription is an object that has access to all the information you provided in the Entity part of the model.
+        // You need it to create an instance of this class.
+        if let ent = NSEntityDescription.entity(forEntityName: "Pin", in: context) {
+            self.init(entity: ent, insertInto: context)
+            self.latitude = latitude
+            self.longitude = longitude
+        } else {
+            fatalError("Unable to find Entity name!")
+        }
+    }
 }
